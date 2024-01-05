73a0518f558b04da2cfba2810c39eecc75e39239
Keith koki <kokikeith@gmail.com>
2024-01-05 10:03:56 +0000

    file fixed

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,27 +7,19 @@ Python script for UTF-8 Validation
 def validUTF8(data):
     # Count of consecutive 1s in the most significant bits
     num_bytes = 0
-
     for num in data:
-        # Check if the current number is a continuation byte
-        if num >> 7 == 1 and (num >> 6) & 1 == 0:
+        binary_rep = format(num, '#010b')[-8:]
+        if num_bytes == 0:
+            for bit in binary_rep:
+                if bit == '0':
+                    break
+                num_bytes += 1
             if num_bytes == 0:
-                return False  # Invalid sequence
-            num_bytes -= 1
-        elif num >> 7 == 0:
-            if num_bytes > 0:
-                return False  # Incomplete sequence
-        elif num >> 6 == 0b10:
-            if num_bytes == 0:
-                return False  # Misplaced continuation byte
-            num_bytes -= 1
-        elif num >> 5 == 0b110:
-            num_bytes = 1
-        elif num >> 4 == 0b1110:
-            num_bytes = 2
-        elif num >> 3 == 0b11110:
-            num_bytes = 3
+                continue
+            if num_bytes == 1 or num_bytes > 4:
+                return False
         else:
-            return False  # Invalid leading byte
-
+            if not (binary_rep[0] == '1' and binary_rep[1] == '0'):
+                return False
+        num_bytes -= 1
     return num_bytes == 0
