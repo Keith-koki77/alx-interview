429223a6a59f83320e3744e287defbc3ff763c97
Keith koki <kokikeith@gmail.com>
2024-02-06 05:25:21 +0000

    file fixed

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,7 +7,32 @@ island described in grid
 
 def island_perimeter(grid):
     perimeter = 0
-    for row in grid + list(map(list, zip(*grid))):
-        for i, j in zip([0] + row, row + [0]):
-            perimeter += int(i != j)
+    rows, cols = len(grid), len(grid[0])
+
+    for x in range(rows):
+        for y in range(cols):
+            if grid[x][y] == 1:
+                if x == 0 or grid[x - 1][y] == 0:
+                    perimeter += 1
+
+                if x == rows - 1 or grid[x + 1][y] == 0:
+                    perimeter += 1
+
+                if y == 0 or grid[x][y - 1] == 0:
+                    perimeter += 1
+
+                if y == cols - 1 or grid[x][y + 1] == 0:
+                    perimeter += 1
+
     return perimeter
+
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print(island_perimeter(grid))
