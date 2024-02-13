f3f692d69d122bba8f7a46e69d39c29711135601
Keith koki <kokikeith@gmail.com>
2024-02-13 05:26:33 +0000

    first commit

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+"""
+
+
+def isWinner(x, nums):
+    """
+    A function that determines the winner
+    of each game.
+    """
+    def is_prime(n):
+        if n < 2:
+            return False
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+    winner = 0
+    for n in nums:
+        if is_prime(n):
+            winner = 1
+        else:
+            winner = 2
+
+    return "Maria" if winner == 1 else "Ben" if winner == 2 else None
