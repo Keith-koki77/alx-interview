bb7b9220f9967ee19f94ad869c22bb1f605e699b
Keith koki <kokikeith@gmail.com>
2024-02-14 10:29:03 +0000

    file fixed

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -9,19 +9,25 @@ def isWinner(x, nums):
     A function that determines the winner
     of each game.
     """
-    def is_prime(n):
-        if n < 2:
-            return False
-        for i in range(2, int(n**0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
+    if x < 1 or not nums:
+        return None
+    maria, ben = 0, 0
 
-    winner = 0
-    for n in nums:
-        if is_prime(n):
-            winner = 1
-        else:
-            winner = 2
+    # Generating prime number max nums
+    num = max(nums)
+    prime = [True for _ in range(1, num + 1, 1)]
+    prime[0] = False
+    for i, isPrime in enumerate(prime, 1):
+        if i == 1 or not isPrime:
+            continue
+        for j in range(i + i, num + 1, i):
+            prime[j - 1] = False
 
-    return "Maria" if winner == 1 else "Ben" if winner == 2 else None
+    # Number of prime less than n in nums
+    for _, num in zip(range(x), nums):
+        count = len(list(filter(lambda x: x, prime[0: num])))
+        ben += count % 2 == 0
+        maria += count % 2 == 1
+    if maria == ben:
+        return None
+    return 'Maria' if maria > ben else 'Ben'
